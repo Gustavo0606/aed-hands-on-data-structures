c0982cd0bfdff8816334c90ec855bf2a81933243
Gustavo0606 <gustavovilelas22@gmail.com>
2026-04-08 09:22:25 -0300

    Implementa MyArray

--- src/my_array.py
+++ src/my_array.py
@@ -10,28 +10,50 @@ class MyArray(Array):
         self.data: list[int] = []
 
     def append(self, value: int) -> None:
-        raise NotImplementedError
+        self.data.append(value)
 
     def get(self, index: int) -> int:
-        raise NotImplementedError
+        if 0 <= index < len(self.data):
+            return self.data[index]
+        else:
+            raise IndexError
 
     def set(self, index: int, value: int) -> None:
-        raise NotImplementedError
+        if 0 <= index < len(self.data):
+            self.data[index] = value
+        else:
+            raise IndexError
 
     def remove(self, value: int) -> None:
-        raise NotImplementedError
+        naoEncontrado = True
+        for i in range(len(self.data)):
+            if self.data[i] == value:
+                self.data.pop(i)
+                naoEncontrado = False
+                break
+        if naoEncontrado:
+            raise ValueError
 
     def insert(self, index: int, value: int) -> None:
-        raise NotImplementedError
+        if 0 <= index <= len(self.data):
+            self.data.insert(index, value)
+        else:
+            raise IndexError
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return len(self.data)
 
     def __getitem__(self, index: int) -> int:
-        raise NotImplementedError
+        if 0 <= index < len(self.data):
+            return self.data[index]
+        else:
+            raise IndexError
 
     def __setitem__(self, index: int, value: int) -> None:
-        raise NotImplementedError
+        if 0 <= index < len(self.data):
+            self.data[index] = value
+        else:
+            raise IndexError
 
     def __repr__(self) -> str:
-        raise NotImplementedError
+        return str(self.data)
