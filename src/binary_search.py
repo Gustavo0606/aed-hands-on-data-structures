8a173d78e055ea1428df08ce377661d8f603baa0
Gustavo0606 <gustavovilelas22@gmail.com>
2026-04-08 09:59:49 -0300

    Implementa binary_search e linear_search

--- src/binary_search.py
+++ src/binary_search.py
@@ -7,4 +7,14 @@ def binary_search(array: MyArray, target: int) -> int:
 
     Deve retornar o índice do elemento ou -1 caso não encontrado.
     """
-    raise NotImplementedError
+    left = 0
+    right = (len(array)-1)
+    while left <= right:
+        meio = (left + right)//2
+        if array[meio] == target:
+            return meio
+        elif array[meio] < target:
+            left = meio + 1
+        else:
+            right = meio - 1
+    return -1
